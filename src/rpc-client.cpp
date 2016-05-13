f85fe506dbd4e31863a75667bac0b74a1cf00dcc
Piotr Walczak <P.Walczak@adbglobal.com>
2016-05-13 11:11:55 +0200

    Init commit for rpc-client
    + adding jsonrpc lib headers
    + adding init version of rpc-client
    + adding CMakeLists.txt

---
+++ src/rpc-client.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include "jsonrpc.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "\nrpc-client v0.1\n";
+    return 0;
+}
